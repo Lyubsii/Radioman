b805e69193ed5fa74d90e855369bd03b6c38a43b
Lyubsii <106180355+Lyubsii@users.noreply.github.com>
2022-12-05 16:56:08 +0300

    Update Main.java

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,58 @@
-package ru.netology;
+package ru.netology.services;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Radio {
+
+
+    private int currentVolume;
+    private int currentStation;
+    int maxVolume = 9;
+    int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void setVolumeDown() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume - 1;
+    }
+
+
+    public void nextStation() {
+        if (currentStation < 9 ) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
     }
 }
